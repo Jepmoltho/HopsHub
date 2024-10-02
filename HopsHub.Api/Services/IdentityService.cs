6aadb2780e64b58f205ba509dd955521aa7fdddc
Jepmoltho <jept@itu.dk>
2024-10-02 14:14:22 +1000

    Refactored identityService and added more seeddata

--- HopsHub.Api/Services/IdentityService.cs
+++ HopsHub.Api/Services/IdentityService.cs
@@ -1,5 +1,5 @@
-﻿using HopsHub.Api.Models;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
+using HopsHub.Api.Constants;
 
 namespace HopsHub.Api.Services;
 
@@ -7,37 +7,38 @@ public class IdentityService
 {
     public static async Task SeedUsers(IServiceProvider serviceProvider)
     {
-        var userManager = serviceProvider.GetRequiredService<UserManager<User>>();
+        var userManager = serviceProvider.GetRequiredService<UserManager<Models.User>>();
+
+        foreach (var user in TestUserConstants.TestUsers)
+        {
+            if (user.Email != null && await userManager.FindByEmailAsync(user.Email) == null)
+            {
+                await CreateTestUser(userManager, user);
+            }
+        }
+    }
+
+    private static async Task CreateTestUser(UserManager<Models.User> userManager, Models.User user)
+    {
+        var result = await userManager.CreateAsync(user, "userPassw0rd!");
+
+        if (result.Succeeded)
+        {
+            await userManager.AddToRoleAsync(user, "User");
+        }
+    }
+
+    public static async Task SeedRoles(IServiceProvider serviceProvider)
+    {
         var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
 
-        // Ensure an admin test role exists
-        //if (!await roleManager.RoleExistsAsync("Admin"))
-        //{
-        //    await roleManager.CreateAsync(new IdentityRole<Guid> { Name = "Admin" });
-        //}
-
-        // Ensure a user test role exists
         if (!await roleManager.RoleExistsAsync("User"))
         {
             await roleManager.CreateAsync(new IdentityRole<Guid> { Name = "User" });
         }
 
-        // Ensure a test user exists
-        if (await userManager.FindByEmailAsync("user1@test.dk") == null)
-        {
-            var user = new User
-            {
-                UserName = "user1",
-                Email = "user1@test.dk",
-                Id = Guid.NewGuid()
-            };
-
-            var result = await userManager.CreateAsync(user, "Test@1234"); // Creates user and hashes the password
-
-            if (result.Succeeded)
-            {
-                await userManager.AddToRoleAsync(user, "User"); // Optionally assign a role
-            }
+        if (!await roleManager.RoleExistsAsync("Admin")) {
+            await roleManager.CreateAsync(new IdentityRole<Guid> { Name = "Admin" });
         }
     }
 }
